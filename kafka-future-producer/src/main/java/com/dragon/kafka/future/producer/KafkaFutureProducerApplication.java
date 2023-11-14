3c47e293b109e2994bf55503078fe598d832d080
chenlong <chenlong@vrvmail.com.cn>
2023-11-14 17:02:25 +0800

    feat(kafka-future-producer):init

---
+++ kafka-future-producer/src/main/java/com/dragon/kafka/future/producer/KafkaFutureProducerApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.kafka.future.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFutureProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFutureProducerApplication.class, args);
+	}
+
+}
