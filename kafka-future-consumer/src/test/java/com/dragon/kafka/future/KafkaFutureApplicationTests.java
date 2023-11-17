c886926a81fb464a40fad37447655a88ec7341ef
cl900503 <1021773811@qq.com>
2023-11-17 11:24:36 +0800

    feat(kafka):update

--- kafka-future-consumer/src/test/java/com/dragon/kafka/future/KafkaFutureApplicationTests.java
+++ kafka-future-consumer/src/test/java/com/dragon/kafka/future/KafkaFutureApplicationTests.java
@@ -1,20 +1,8 @@
 package com.dragon.kafka.future;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.dragon.kafka.future.producer.Producer;
 
 @SpringBootTest
 class KafkaFutureApplicationTests {
-
-	@Autowired
-	private Producer producer;
-	
-	@Test
-	void contextLoads() {
-		producer.sendMessage();
-	}
 
 }
